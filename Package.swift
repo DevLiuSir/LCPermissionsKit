63eca50dae2b6e787a177d168151b08a441d2993
Marvin <93428739@qq.com>
2024-12-25 09:30:03 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,10 +13,6 @@ let package = Package(
             name: "LCPermissionsKit",
             targets: ["LCPermissionsKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         .target(
             name: "LCPermissionsKit",
