d05fdb73fe323b380587c8020f90e783efa84523
Marvin <93428739@qq.com>
2024-12-25 09:26:19 +0800

    Specifying a minimum version

--- Package.swift
+++ Package.swift
@@ -1,12 +1,14 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LCPermissionsKit",
+    platforms: [
+        .macOS(.v10_15) // 指定支持的 macOS 版本
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "LCPermissionsKit",
             targets: ["LCPermissionsKit"]),
@@ -16,8 +18,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "LCPermissionsKit",
             dependencies: []),
